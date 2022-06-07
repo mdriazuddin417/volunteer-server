ff4697b236b5ea000aff66a5d6c9b3ed3e04c09e
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-06-07 06:20:02 +0600

    env file update

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://volunTeer:GLKpDWudG1c94VjW@cluster0.gkpf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.gkpf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
